d30e05352faa416364d71a9bcf910df0cecee513
Charlotte Head <72808053+charlierose22@users.noreply.github.com>
2023-04-30 08:05:27 +0100

    finalise graph for presentation

--- moisture-content/moisture-content-visualisation.R
+++ moisture-content/moisture-content-visualisation.R
@@ -8,8 +8,8 @@ moisture_data <- readxl::read_excel("Moisture Content Analysis.xlsx",
 # change length to location A and B.
 moisture_renamed <- mutate(moisture_data,
                             pile_length = case_when(
-                              str_detect(sample, "Quarter") ~ "B",
-                              str_detect(sample, "Half") ~ "A"))
+                              str_detect(pile_length, "Quarter") ~ "B",
+                              str_detect(pile_length, "Half") ~ "A"))
 
 # simple graph
 moisture_renamed %>%
@@ -21,5 +21,5 @@ ggplot(aes(x = date, y = moisture_content_pc,
   theme_light(base_size = 20) +
   labs(x = "Time",
        y = "% Moisture Content",
-       colour = "Height",
-       shape = "Length")
+       colour = "Pile Height",
+       shape = "Location")
