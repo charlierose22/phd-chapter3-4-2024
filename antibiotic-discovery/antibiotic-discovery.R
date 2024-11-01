fabb3fd54c7f22fd93564f3d6e729dd7c3e802a1
Charlotte Head <72808053+charlierose22@users.noreply.github.com>
2024-11-01 18:59:22 +0000

    update timeline

--- antibiotic-discovery/antibiotic-discovery.R
+++ antibiotic-discovery/antibiotic-discovery.R
@@ -1,18 +1,22 @@
+library(tidyverse)
 library(grafify)
 
 # import dataset
 discovery_timeline <- readxl::read_excel("antibiotic-discovery/antibiotic-discovery.xlsx", 
-                                         col_types = c("text", "date"))
+                                         col_types = c("text", "date", "text"))
 
 # mutate year column to years with lubridate
 discovery_timeline$year <- lubridate::as_date(discovery_timeline$year)
+discovery_timeline$year2 <- lubridate::year(discovery_timeline$year)
 
 # plot
 discovery_timeline %>%
-  ggplot(aes(x = year, y = 0, colour = class)) +
-  geom_point(shape = 15) +
-  geom_line() +
-  labs(x = "Year", y = "", colour = "Antibiotic Class") +
-  scale_x_date(date_breaks = "5 years", date_labels = "%y") +
-  theme_minimal(base_size = 12) +
-  theme(legend.position = "bottom")
+  ggplot(aes(x = year, y = 0.5, colour = class, label = class)) +
+  geom_point(shape = 16, size = 2) +
+  geom_text(aes(label = class), nudge_y = 0.25, size = 4, angle = 90) +
+  geom_label(aes(label = year2), nudge_y = -0.08, size = 4, angle = 90) +
+  labs(x = "Year", y = "") +
+  scale_x_date(date_breaks = "5 years", date_labels = "%Y") +
+  scale_y_discrete(labels = "") +
+  theme_void(base_size = 12) +
+  theme(legend.position = "none")
